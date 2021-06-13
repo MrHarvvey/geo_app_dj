937fdac2e5c7a073db4906725bf49a826b1295a0
MrHarvvey <oskarflorczak@gmail.com>
2021-06-13 23:23:51 +0200

    firs test

--- geo_app/tests.py
+++ geo_app/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+import requests
+import json
 
-# Create your tests here.
+def street_search_req(city):
+    data ={
+    "city": city
+    }
+    headers = {'Content-type': 'application/json'}
+    req = requests.post("http://127.0.0.1:8000/street/", data=json.dumps(data), headers=headers)
+    return req.text
+
+
+for i in range(10000):
+    print(street_search_req('Bolimów'))
